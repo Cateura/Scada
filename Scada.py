5fc60418ca264205349a6f9de2635b74f5731a5a
Cateura <93153637+Cateura@users.noreply.github.com>
2022-05-17 22:36:18 +0200

    html

--- Scada.py
+++ Scada.py
@@ -1,2 +1,19 @@
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+pins = (11,12) #pins is a dict
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pins, GPIO.OUT)
+GPIO.output(pins, GPIO.LOW)
+
+p_R = GPIO.PWM(pins[0], 2000)
+p_G = GPIO.PWM(pins[1], 2000)
+
+
+
+
 
 print("hello World!")
